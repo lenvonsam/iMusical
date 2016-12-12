4f3ab25d33f8bca4241869f04e94cee70e661712
Coolar <coolar@eurus.cn>
2016-12-12 10:07:30 +0800

    fix ios bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,28 @@
+//内置模块
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { HttpModule } from '@angular/http';
+//页面
 import { MyApp } from './app.component';
-import pages from './components';
+import { DiscoverPage } from '../pages/discover/discover';
+import { MePage } from '../pages/me/me';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ConsultDetailPage } from '../pages/home/consultDetail';
+import { LastShowPage } from '../pages/home/lastShow';
+import {HomePagePop} from '../pages/home/homePopPage';
+import {DiscoverDetailPage} from '../pages/discover/discoverDetail';
+import {MeLoginPage} from '../pages/me/login';
+import {PageForgetPwdStep1} from '../pages/me/forgetPwdStep1';
+import {PageForgetPwdStep2} from '../pages/me/forgetPwdStep2';
+import {PageForgetPwdStep3} from '../pages/me/forgetPwdStep3';
+import {CaptchaComp} from '../directives/captcha';
+import {PageRegisterStep1} from '../pages/me/registerStep1';
+import {PageRegisterStep2} from '../pages/me/registerStep2';
+import {CommentCreatePage} from '../pages/home/commentCreate';
+import {CommentListPage} from '../pages/home/commentList';
+import {PageProfile} from '../pages/me/profile';
 
-// import { MusicalHttpService } from '../services/http-service';
 //视频插件
 // import {BrowserModule} from '@angular/platform-browser';
 // import {VgCore} from 'videogular2/core';
@@ -13,7 +31,27 @@ import pages from './components';
 // import {VgBufferingModule} from 'videogular2/buffering';
 
 @NgModule({
-  declarations: pages,
+  declarations: [
+  MyApp,
+  DiscoverPage,
+  MePage,
+  HomePage,
+  TabsPage,
+  ConsultDetailPage,
+  LastShowPage,
+  HomePagePop,
+  DiscoverDetailPage,
+  MeLoginPage,
+  PageForgetPwdStep1,
+  PageForgetPwdStep2,
+  PageForgetPwdStep3,
+  CaptchaComp,
+  PageRegisterStep1,
+  PageRegisterStep2,
+  CommentCreatePage,
+  CommentListPage,
+  PageProfile
+  ],
   imports: [
     HttpModule,
     // BrowserModule,
@@ -30,7 +68,27 @@ import pages from './components';
     })
   ],
   bootstrap: [IonicApp],
-  entryComponents: pages,
+  entryComponents: [
+  MyApp,
+  DiscoverPage,
+  MePage,
+  HomePage,
+  TabsPage,
+  ConsultDetailPage,
+  LastShowPage,
+  HomePagePop,
+  DiscoverDetailPage,
+  MeLoginPage,
+  PageForgetPwdStep1,
+  PageForgetPwdStep2,
+  PageForgetPwdStep3,
+  CaptchaComp,
+  PageRegisterStep1,
+  PageRegisterStep2,
+  CommentCreatePage,
+  CommentListPage,
+  PageProfile
+  ],
   providers: []
 })
 export class AppModule {}
